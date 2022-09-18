afa4e6a6625baead7103a8f0025a17d166fabb67
JanaZhakPL <janazhakpl@gmail.com>
2022-09-18 12:03:40 +0200

    **RotateArray method changed but still not cyclic  the shift is limited by the lengt of the array)

--- homework/src/main/java/lesson2/RotateArray.java
+++ homework/src/main/java/lesson2/RotateArray.java
@@ -1,7 +1,5 @@
 package lesson2;
 
-import static lesson2.MainApp.printArrayInConsole;
-
 public class RotateArray {
     public static int[] shiftArray(int[] arr, int n) {
         int[] b = new int[arr.length];
@@ -11,8 +9,9 @@ public class RotateArray {
                 System.arraycopy(arr, 0, b, n, arr.length - n);
                 System.arraycopy(arr, arr.length - n, b, 0, n);
             } else {
-                System.arraycopy(arr, Math.abs(n), b, 0, arr.length + n);
-                System.arraycopy(arr, 0, b, arr.length + n, Math.abs(n));
+                n*=-1;
+                System.arraycopy(arr, n, b, 0, arr.length - n);
+                System.arraycopy(arr, 0, b, arr.length - n, n);
             }
             return b;
         }
