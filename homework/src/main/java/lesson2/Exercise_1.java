a2d64f464c99c5ccc30f8187f6e081d5d48f41d1
JanaZhakPL <janazhakpl@gmail.com>
2022-09-21 21:42:41 +0200

    Homework 3 Checker's comments noted

--- homework/src/main/java/lesson2/Exercise_1.java
+++ homework/src/main/java/lesson2/Exercise_1.java
@@ -4,6 +4,7 @@ package lesson2;
  * Написать метод, принимающий на вход два целых числа и проверяющий,
  * что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
  */
+
 public class Exercise_1 {
     public static boolean within10and20(int a, int b) {
         return a + b >= 10 && a + b <= 20;
